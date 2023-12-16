b7e78f67b53257408bac0df8f895b3c27bb46a1b
Justine_24 <justine.xiang@ensea.fr>
2023-12-16 23:40:24 +0100

    update

--- src/GameScene.java
+++ src/GameScene.java
@@ -4,41 +4,47 @@ import javafx.scene.Scene;
 import java.io.FileNotFoundException;
 
 public class GameScene extends Scene {
-    private static final double HERO_START_X = 200;
-    private static final double HERO_START_Y = 300;
+
     //constructor
     public GameScene(Parent parent, double v, double v1, boolean b) throws FileNotFoundException {
         super(parent, v, v1, b);
-        //createGameLoop();
     }
 
     //int numberOfLives = 3;
     static Camera camera = new Camera(0, 0);
     public static StaticThing rightBackground;
-    public static StaticThing leftBackground;
-    static Hero hero;
 
     static {
         try {
             rightBackground = new StaticThing(800, 0, "..\\Runner_2\\img\\desert.png");
-            leftBackground = new StaticThing(0, 0, "..\\Runner_2\\img\\desert.png");
-            //hero = new Hero(HERO_START_X, HERO_START_Y, 0, "file:.\\path\\to\\heros.png", 7, 100, 65, 20);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
-    /*private void createGameLoop() {
-        AnimationTimer timer = new AnimationTimer() {
-            public void handle(long time) {
-                update(time);
-                render();
-            }
-        };
-        timer.start();
+
+    public static StaticThing leftBackground;
+
+    static {
+        try {
+            leftBackground = new StaticThing(0, 0, "..\\Runner_2\\img\\desert.png");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void update(long time) {
     }
+
+    static Hero hero;
+
+    /*static {
+        try {
+            hero = new Hero(200, 300, 0,"..\\Runner_2\\img\\heros.png");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }*/
+
     AnimationTimer timer = new AnimationTimer() {
         public void handle(long time) {
             hero.update(time);
@@ -47,7 +53,7 @@ public class GameScene extends Scene {
         }
 
 
-    };*/
+    };
 
     //render method
     public static void render() {
